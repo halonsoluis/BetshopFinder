6f2f66ad2cbc15a8e3f54ceadef5fb9880d7465d
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2022-01-25 09:13:09 +0100

    Initial Commit

---
+++ betshops/betshopsApp.swift
@@ -0,0 +1,17 @@
+//
+//  betshopsApp.swift
+//  betshops
+//
+//  Created by Hugo Alonso on 25/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct betshopsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
