9753af6b0563f3bd008e4dae5c21bc5786f39119
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2022-01-27 00:35:28 +0100

    Add initial and temporal test to check that the API is working properly and that the parsing is OK

--- BetshopAPITests/APIDataParserTests.swift
+++ BetshopAPITests/APIDataParserTests.swift
@@ -21,4 +21,18 @@ class APIDataParserTests: XCTestCase {
         XCTAssertEqual(betshops.count, 3)
     }
 
+    func test_API_works() throws {
+        var components = URLComponents()
+            components.scheme = "https"
+            components.host = "interview.superology.dev"
+            components.path = "/betshops"
+            components.queryItems = [
+                URLQueryItem(name: "boundingBox", value: "48.16124,11.60912,48.12229,11.52741"),
+            ]
+
+        let betshops = try APIDataParser().decode(dataURL: components.url!)
+
+        XCTAssertEqual(betshops.count, 109)
+    }
+
 }
