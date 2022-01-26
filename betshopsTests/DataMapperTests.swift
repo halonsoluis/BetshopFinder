c51132b08fa61b6cf78a8b1ef9386383737c9b25
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2022-01-26 22:51:19 +0100

    Map data from decodable representation to the model

--- betshopsTests/DataMapperTests.swift
+++ betshopsTests/DataMapperTests.swift
@@ -9,14 +9,47 @@ import XCTest
 @testable import betshops
 
 struct DataMapper {
-
+    static func buildModel(from data: DecodedBetshop) -> BetshopModel {
+        BetshopModel(
+            id: data.id,
+            name: data.name,
+            address: data.address,
+            city: data.city,
+            county: data.county,
+            location: (data.location.lat, data.location.lng)
+        )
+    }
 }
 
 class DataMapperTests: XCTestCase {
 
-    func test_mapper_init() throws {
-        let sut = DataMapper()
+    func testMapper_createsBetshopModel_withValidData() {
+        let betshopData = exampleData
+        let betshop = DataMapper.buildModel(from: betshopData)
 
-        XCTAssertNotNil(sut)
+        XCTAssertEqual(betshop.id, betshopData.id)
+        XCTAssertEqual(betshop.name, betshopData.name)
+        XCTAssertEqual(betshop.address, betshopData.address)
+        XCTAssertEqual(betshop.city, betshopData.city)
+        XCTAssertEqual(betshop.county, betshopData.county)
+        XCTAssertEqual(betshop.location.lat, betshopData.location.lat)
+        XCTAssertEqual(betshop.location.lng, betshopData.location.lng)
+    }
+}
+
+extension DataMapperTests {
+    var exampleData: DecodedBetshop {
+        DecodedBetshop(
+            name: "Lenbachplatz 7",
+            location: GeoLocation(
+                lng: 11.5689638,
+                lat: 48.1405515
+            ),
+            id: 2350329,
+            county: "Bayern",
+            city_id: 80333,
+            city: "Muenchen",
+            address: "80333 Muenchen"
+        )
     }
 }
