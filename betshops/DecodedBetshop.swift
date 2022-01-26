04764ff695b2bd5a0b5a1ecd91819944d6d91e63
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2022-01-26 22:08:04 +0100

    Add decodable for parsing data from the API for retrieving Betshops

---
+++ betshops/DecodedBetshop.swift
@@ -0,0 +1,24 @@
+//
+//  DecodedBetshop.swift
+//  betshops
+//
+//  Created by Hugo Alonso on 26/01/2022.
+//
+
+import Foundation
+
+struct DecodedBetshop: Decodable {
+    let name: String
+    let location: GeoLocation
+    let id: Int64
+    let county: String
+    let city_id: Int64
+    let city: String
+    let address: String
+
+}
+
+struct GeoLocation: Decodable {
+    let lng: Double
+    let lat: Double
+}
