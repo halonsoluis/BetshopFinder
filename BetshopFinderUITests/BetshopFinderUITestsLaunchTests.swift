fb3ba8b9c91ee329d84b875ddf486bf810b284f3
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2022-01-29 00:25:08 +0100

    Add a project that will be entirely based on UIKit (as for avoiding unnecessary complications, SwiftUI seems quite buggy and incomplete yet)

---
+++ BetshopFinderUITests/BetshopFinderUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BetshopFinderUITestsLaunchTests.swift
+//  BetshopFinderUITests
+//
+//  Created by Hugo Alonso on 29/01/2022.
+//
+
+import XCTest
+
+class BetshopFinderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
