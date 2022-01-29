3ce05aae5d66f9fbbb683b58dfc9154eab89e7bd
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2022-01-29 01:10:11 +0100

    Setup the MapViewViewModel to be used by the map view

---
+++ BetshopFinder/UI/MapViewPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  MapViewPresenter.swift
+//  BetshopFinder
+//
+//  Created by Hugo Alonso on 29/01/2022.
+//
+
+import Foundation
---
+++ BetshopFinder/UI/MapViewViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  MapViewViewModel.swift
+//  BetshopFinder
+//
+//  Created by Hugo Alonso on 29/01/2022.
+//
+
+import Foundation
+import MapKit
+
+class MapViewViewModel {
+
+    var annotations: [Betshop] = []
+    var mapRegion = MKCoordinateRegion()
+    var selected: Betshop?
+}
+
+extension MapViewViewModel {
+
+    private static let munich = CLLocationCoordinate2D(
+        latitude: 48.137154,
+        longitude: 11.576124
+    )
+    private static let defaultSpan = MKCoordinateSpan(
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+    )
+
+    static func defaultMunichLocation() -> MapViewViewModel {
+        let map = MapViewViewModel()
+        map.mapRegion = MKCoordinateRegion(
+            center: munich,
+            span: defaultSpan
+        )
+        return map
+    }
+}
