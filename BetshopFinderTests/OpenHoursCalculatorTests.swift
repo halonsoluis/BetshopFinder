3be01ae693c4d751495b1bfaedea3e7246211155
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2022-01-29 18:21:56 +0100

    Display open until ending hour

--- BetshopFinderTests/OpenHoursCalculatorTests.swift
+++ BetshopFinderTests/OpenHoursCalculatorTests.swift
@@ -9,13 +9,17 @@ import XCTest
 
 enum OpenHoursCalculator {
     static func storeStatus(now: NSDate = NSDate(), workingHours: (opening: String, closing: String)) -> String {
-        return "Open Now"
+        return "Open Now until \(workingHours.closing)"
     }
 }
 
 class OpenHoursCalculatorTests: XCTestCase {
 
     func test_openHours_showOpenNowWhenInBetweenWorkingHours() throws {
-        XCTAssertEqual(OpenHoursCalculator.storeStatus(workingHours: (opening: "8:00", closing: "20:00")), "Open Now")
+        XCTAssertEqual(OpenHoursCalculator.storeStatus(workingHours: (opening: "08:00", closing: "20:00")), "Open Now until 20:00")
     }
+
+//    func test_openHours_showOpensTomorrowWhenAfterWorkingHours() throws {
+//        XCTAssertEqual(OpenHoursCalculator.storeStatus(workingHours: (opening: "08:00", closing: "20:00")), "Opens tomorrow at 08:00")
+//    }
 }
